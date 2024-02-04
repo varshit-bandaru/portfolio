f7b17fa22037146148a905856e0300287319a7b2
varshit.b <varshit.b@abhibus.com>
2024-02-04 19:32:12 +0530

    wokred on the multiple componenents

---
+++ components/sections/hero/Intro.tsx
@@ -0,0 +1,15 @@
+import List from '@/components/ui/List';
+import { Eye, History, ThumbsUp } from 'lucide-react';
+import { FC } from 'react';
+
+interface IntroProps {}
+
+const Intro: FC<IntroProps> = ({}) => {
+    return (
+        <div className="flex flex-1 flex-col md:flex-row gap-7">
+            {/*  */}
+        </div>
+    );
+};
+
+export default Intro;
